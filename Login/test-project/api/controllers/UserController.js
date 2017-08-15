274285b68b11ef8484a728675b04e265f3bb764f
mordekai23 <USRDEL@USR20.local>
2017-08-14 19:33:41 -0500

    login

---
+++ Login/test-project/api/controllers/UserController.js
@@ -0,0 +1,106 @@
+/**
+ * UsuarioController
+ *
+ * @description :: Server-side logic for managing Usuarios
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	'new': function (req, res) {
+
+    res.view();
+
+  },
+
+  create: function (req, res, next) {
+	  User.create( req.params.all(), function  userCreated(err, user) {
+
+
+      if(err) {
+        console.log(err);
+        req.session.flash = {
+          err: err
+        }
+
+
+        return res.redirect('/user/new');
+      }
+
+      req.session.authenticated = true;
+      req.session.User = user;
+
+      //res.json(user);
+      res.redirect('/user/show/'+user.id);
+
+    });
+
+  },
+  show: function(req, res, next) {
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) return next(err);
+      if (!user) return next();
+      console.log(user);
+      res.view({
+        user: user
+      });
+    });
+  },
+  index: function(req, res, next) {
+
+    // Get an array of all users in the User collection(e.g. table)
+    User.find(function foundUsers(err, users) {
+      if (err) return next(err);
+      // pass the array down to the /views/index.ejs page
+      res.view({
+        users: users
+      });
+    });
+  },
+  // render the edit view (e.g. /views/edit.ejs)
+  edit: function(req, res, next) {
+
+    // Find the user from the id passed in via params
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) return next(err);
+      if (!user) return next('User doesn\'t exist.');
+
+      res.view({
+        user: user
+      });
+    });
+  },
+  update: function(req, res, next) {
+    User.update(req.param('id'), req.params.all(), function userUpdated(err) {
+      if(err){
+        return res.redirect('/user/edit/' + req.param('id'));
+      }
+      res.redirect('/user/show/' + req.param('id'));
+    });
+  },
+  destroy: function(req, res, next) {
+
+    User.findOne(req.param('id'), function foundUser(err, user) {
+      if (err) return next(err);
+
+      if (!user) return next('User doesn\'t exist.');
+
+      User.destroy(req.param('id'), function userDestroyed(err) {
+        if (err) return next(err);
+
+        // Inform other sockets (e.g. connected sockets that are subscribed) that this user is now logged in
+        User.publishUpdate(user.id, {
+          name: user.name,
+          action: ' has been destroyed.'
+        });
+
+        // Let other sockets know that the user instance was destroyed.
+        User.publishDestroy(user.id);
+
+      });
+
+      res.redirect('/user');
+
+    });
+  }
+};
+
